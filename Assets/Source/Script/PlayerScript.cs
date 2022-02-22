115f829b490d5f2fd93eb13c315fb0a6bb087b36
mywatt <padamu1@naver.com>
2022-02-22 15:31:14 +0900

    add movement system

--- Assets/Source/Script/PlayerScript.cs
+++ Assets/Source/Script/PlayerScript.cs
@@ -5,19 +5,62 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     new Rigidbody2D rigidbody2D;
-
+    new Transform transform;
+    float m_fSpeed;
     void Awake()
     {
         rigidbody2D = this.GetComponent<Rigidbody2D>();
+        transform = this.GetComponent<Transform>();
+        m_fSpeed = 500f;
     }
 
     void Start()
     {
-        rigidbody2D.AddForce(transform.up * -10f);
     }
-
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (rigidbody2D.velocity.x > -3f)
+            {
+                rigidbody2D.AddForce(new Vector2(-1f, 0f) * Time.deltaTime * m_fSpeed, ForceMode2D.Force);
+            }
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (rigidbody2D.velocity.x < 3f)
+            {
+                rigidbody2D.AddForce(new Vector2(1f, 0f) * Time.deltaTime * m_fSpeed, ForceMode2D.Force);
+            }
+        }
+
+        if (rigidbody2D.velocity.x > 0f)
+        {
+            rigidbody2D.AddForce(new Vector2(-0.5f, 0f) * Time.deltaTime * m_fSpeed, ForceMode2D.Force);
+        }
+        else if (rigidbody2D.velocity.x < 0f)
+        {
+            rigidbody2D.AddForce(new Vector2(0.5f, 0f) * Time.deltaTime * m_fSpeed, ForceMode2D.Force);
+        }
+        if(rigidbody2D.velocity.y < 0f)
+        {
+            rigidbody2D.AddForce(new Vector2(0f, -5f) * Time.deltaTime * m_fSpeed, ForceMode2D.Force);
+        }
+        else
+        {
+            rigidbody2D.AddForce(new Vector2(0f, -5f) * Time.deltaTime * m_fSpeed, ForceMode2D.Force);
+        }
+
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("MapObject"))
+        {
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0f);
+            //Vector3 vector = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.right;
+            rigidbody2D.AddForce(new Vector2(0f, 900f), ForceMode2D.Force);
+        }
+    }
+
 }
